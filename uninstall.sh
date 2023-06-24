afaf791af5ac72d5108a73f5754639b0a7897cf5
CommentSectionScientist <matthias-oswald@gmx.net>
2023-06-24 15:14:46 +0200

    rename to dbus-evcharger-openwb; removed files

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SERVICE_NAME=$(basename $SCRIPT_DIR)
 
 rm /service/$SERVICE_NAME
-kill $(pgrep -f 'supervise dbus-evsecharger')
+kill $(pgrep -f 'supervise dbus-evcharger-openwb')
 chmod a-x $SCRIPT_DIR/service/run
 ./restart.sh
